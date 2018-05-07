5b488c407f9b2ebf7984ce3321f01482f8458fd7
Jisargu <jonathnsaa@gmail.com>
2018-05-07 13:39:56 -0500

    Eliminando el hardcodeo

--- js/switch.js
+++ js/switch.js
@@ -1,24 +1,37 @@
 'use strict';
-const CURRENT_YEAR=2018;
+const CURRENT_YEAR = 2018;
+const EDAD_RAMSES = 24;
+const EDAD_JONATHAN = 27;
+const EDAD_JESSIE = 35;
+const EDAD_LIZ = 36;
+const EDAD_JORGE = 32;
+const EDAD_RICARDO = 25; //En realidad 24 pero para que nos e repitan los casos
+const EDAD_JAIR_CHAN = 31;
+
 function switchu() {
-    let edad = prompt("Escribe una edad"); /*promt retorna un string*/
+    let edad = Number(prompt("Escribe una edad")); /*promt retorna un string.
+    Se puede usar parseInt() para transformar el resultado en int 
+    o Number().
+    La diferencia es que parse solo toma el numero pero si inicia con 0 se transforma en octal.
+    Number() solo toma numeros y si tienen letras o notaciones manda NaN
+    */
     let year_of_birth = anioNacimiento(edad);
     switch (edad) { /*Los strings van entre comillas y en los valores numéricos no son necesarios */
-        case '24': alert("es la de ramses" + " y el año de nacimiento es: " + year_of_birth);
+        case EDAD_RAMSES: alert("es la de ramses" + " y el año de nacimiento es: " + year_of_birth);
             break;
-        case '27': alert("es la mia" + " y el año de nacimiento es: " + year_of_birth);
+        case EDAD_JONATHAN: alert("es la mia" + " y el año de nacimiento es: " + year_of_birth);
             break;
-        case '30': alert("es la de Jessie" + " y el año de nacimiento es: " + year_of_birth);
+        case EDAD_JESSIE: alert("es la de Jessie" + " y el año de nacimiento es: " + year_of_birth);
             break;
-        case '36': alert("es la de Liz" + " y el año de nacimiento es: " + year_of_birth);
+        case EDAD_LIZ: alert("es la de Liz" + " y el año de nacimiento es: " + year_of_birth);
             break;
-        case '32': alert("es la de Jorge" + " y el año de nacimiento es: " + year_of_birth);
+        case EDAD_JORGE: alert("es la de Jorge" + " y el año de nacimiento es: " + year_of_birth);
             break;
-        case '25': alert("es la de Ricardo" + " y el año de nacimiento es: " + year_of_birth);
+        case EDAD_RICARDO: alert("es la de Ricardo" + " y el año de nacimiento es: " + year_of_birth);
             break;
-        case '31': alert("es la de Chan" + " y el año de nacimiento es: " + year_of_birth);
+        case EDAD_JAIR_CHAN: alert("es la de Chan" + " y el año de nacimiento es: " + year_of_birth);
             break;
-        default: alert("No hay nadie con esa" + " y el año de nacimiento es: " + year_of_birth);
+        default: alert("No hay nadie con esa edad");
     }
 }
 
