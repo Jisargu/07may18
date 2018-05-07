a8896550cbb8dace0df0325eea54332622756436
Jisargu <jonathnsaa@gmail.com>
2018-05-07 15:23:34 -0500

    pequeños cambios en las funciones

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 function menu() {
     do {
         var opcion = prompt("Menú: \n1.-Sumar 2 números\n2.-Restar 2 números\n3.-Tabla de multiplicar primeros 10 números\n4.-Salir");
-        var tabla="";
+        var tabla = "";
         switch ((Number(opcion))) {
 
             case 1: suma();
@@ -20,20 +20,20 @@ function menu() {
         }
     } while (Number(opcion) != 4);
 }
-function suma(){
+function suma() {
     let numero1 = Number(prompt("Escribe el primer número para sumar"));
     let numero2 = Number(prompt("Escribe el segundo número"));
     alert("la suma es: " + (numero1 + numero2));
 }
-function resta(){
+function resta() {
     let numero1 = Number(prompt("Escribe el primer número para restar"));
     let numero2 = Number(prompt("Escribe el segundo número"));
     alert("la resta es: " + (numero1 - numero2));
 }
-function tabla_multiplicar(){
+function tabla_multiplicar() {
     let numero1 = Number(prompt("Que tabla de multiplicar"));
-                for(i=1;i<=10;i++){
-                    tabla+=(numero1*i+"\n");
-                }
-                alert(tabla);
+    for (i = 1; i <= 10; i++) {
+        tabla += (numero1 * i + "\n");
+    }
+    alert(tabla);
 }
